f3ca114ea614d899cbfa093632aa6a5d54658427
venkatvallabhaneni <venkat.vallabhaneni@thoughtworks.com>
2021-11-25 12:07:44 +0530

    elastic search consumer for twitter

--- kafka-consumer-elastic/src/main/java/com/venkat/kafka/ElasticSearchConsumer.java
+++ kafka-consumer-elastic/src/main/java/com/venkat/kafka/ElasticSearchConsumer.java
@@ -6,19 +6,33 @@ import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestClientBuilder;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
 
 public class ElasticSearchConsumer {
 
-    public static void main(String[] args) {
+    private static final Logger logger = LoggerFactory.getLogger(ElasticSearchConsumer.class);
 
+    public static void main(String[] args) throws IOException {
 
+        RestHighLevelClient client = createElkClient();
+        IndexRequest indexRequest = new IndexRequest("twitter-venkat").source("{\"name\":\"venkat\"}", XContentType.JSON);
+        IndexResponse indexResponse = client.index(indexRequest, RequestOptions.DEFAULT);
+        logger.info("Index created  Id : " + indexResponse.getId() + " status " + indexResponse.status());
+        client.close();
 
     }
 
-    private static RestHighLevelClient createElkClient () {
+    private static RestHighLevelClient createElkClient() {
 
         CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
         credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(AppPropertyReader.getValue("elastic.username"), AppPropertyReader.getValue("elastic.password")));
